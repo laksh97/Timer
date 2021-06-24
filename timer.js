17ea30add8816481a7b89317ab09c4fc6c588c17
laksh97 <lakshlj816@gmail.com>
2021-06-24 23:07:58 +0530

    first commit

---
+++ timer.js
@@ -0,0 +1,46 @@
+var h1 = document.getElementById("htext");
+setTimeout(function () {
+    h1.innerText = "Happy Birthday!"
+}, 11000);
+setTimeout(function () {
+    h1.innerText = "1"
+}, 10000);
+setTimeout(function () {
+    h1.innerText = "2"
+}, 9000);
+setTimeout(function () {
+    h1.innerText = "3"
+}, 8000);
+setTimeout(function () {
+    h1.innerText = "4"
+}, 7000);
+setTimeout(function () {
+    h1.innerText = "5"
+}, 6000);
+setTimeout(function () {
+    h1.innerText = "6"
+}, 5000);
+setTimeout(function () {
+    h1.innerText = "7"
+}, 4000);
+setTimeout(function () {
+    h1.innerText = "8"
+}, 3000);
+setTimeout(function () {
+    h1.innerText = "9"
+}, 2000);
+setTimeout(function () {
+    h1.innerText = "10"
+}, 1000);
+document.body.appendChild(h1)
+// var downloadTimer = setTimeout(function(){
+// var countdown = 10;
+//     while(countdown >  0) {
+//         h1.innerText =  countdown + " seconds";
+//         document.body.appendChild(h1)
+//         countdown--;
+//     }
+//     h1.innerText = "Happy Birthday!"
+//     document.body.appendChild(h1)
+
+// }, 1000);
